1160057994722020e42afd08e225958ab4738802
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-05-12 17:17:08 +0200

    Added Meaning component

--- src/Results.js
+++ src/Results.js
@@ -1,8 +1,22 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
   if (props.result) {
-    return <div className="results mt-5"> Hey</div>;
+    return (
+      <div className="results fs-3 mt-5">
+        <h2>{props.result.word}</h2>
+        <div className="definition">
+          {props.result.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
   } else {
     return null;
   }
