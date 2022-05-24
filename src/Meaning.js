43fec4b6c62e42aff90294af3f0c91ed37b19c02
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-05-24 12:37:33 +0200

    Rendered the word meanings

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-  return "Hello from here";
+  return (
+    <div className="meaniing justify-content-start fs-4 fw-normal mb-4">
+      <div className="row fw-bold mb-3">{props.meaning.partOfSpeech}</div>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div className="meaning-definition justify-content-start fw-light fs-5 lh-sm">
+            <div key={index}>
+              <p>{definition.definition}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
