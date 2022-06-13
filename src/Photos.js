996dc920695d441ea066541a82aa63c36240cbb2
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-06-13 19:50:31 +0200

    Created Photos component

---
+++ src/Photos.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <div className="row">
+        <div className="col-10 mx-auto">
+          <div className="Photos">
+            <div className="main-search card shadow p-3 mt-3 mb-5 bg-body rounded">
+              <div className="row">
+                {props.photos.map(function (photo, index) {
+                  return (
+                    <div className="col-md-4 mb-2 p-2 mx-auto">
+                      <a
+                        href={photo.src.original}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={index}
+                      >
+                        <img
+                          src={photo.src.landscape}
+                          alt="search-pic"
+                          className="img-fluid d-inline"
+                        />
+                      </a>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
